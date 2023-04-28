80cfcc22726c5734d02d0f79203dd08ac927b62f
shinjons99 <shinjons99@gmail.com>
2023-04-28 08:53:16 +0600

    Create News and category data loading API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 
 const catagories = require('./Data/Catagories.json');
+const news = require('./Data/news.json');
 
 app.use(cors());
 app.get('/', (req, res)=>{
@@ -15,7 +16,29 @@ app.get('/catagories',(req, res)=>{
     console.log(catagories);
     res.send(catagories);
 })
+app.get('/news', (req, res)=>{
+    res.send(news);
+})
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n=>n._id === id);
+    res.send(selectedNews);
+})
 
+app.get('/catagories/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+
+        const categoryNews = news.filter(n=>parseInt(n.category_id) === id);
+        console.log(categoryNews);
+    }
+    
+})
 
 app.listen(port, ()=>{
     console.log(`Dragon API is running on port: ${port}`);
